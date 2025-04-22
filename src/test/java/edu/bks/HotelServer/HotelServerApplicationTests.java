650211f76ff43c0f9ed639e81d0ee2dafa67da06
Bhanukakumara <bhanukarasika@gmail.com>
2025-04-22 14:25:54 +0530

    initialization

---
+++ src/test/java/edu/bks/HotelServer/HotelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bks.HotelServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
